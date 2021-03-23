0af4d01cdf2cd8e1223f51b579d4e17ec239a736
Gabriel <gabriel.simourafox@gmail.com>
2021-03-23 10:46:28 -0300

    Alterei IdadeCandidatos

--- Eleicao.java
+++ Eleicao.java
@@ -267,14 +267,3 @@ public class Eleicao {
     }
   }
 }
-
-class AgeCalculator {
-
-  public static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
-    if ((birthDate != null) && (currentDate != null)) {
-      return Period.between(birthDate, currentDate).getYears();
-    } else {
-      return 0;
-    }
-  }
-}
